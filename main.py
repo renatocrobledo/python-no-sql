5ea6a47307da337680be549ac39b37980e6cdff8
Renato <renatocrobledo@gmail.com>
2018-06-27 21:59:42 -0500

    first example

---
+++ main.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.testing_database
+myFirstCollection = db.myFirstCollection
+
+document = {
+  "nam": "TOROMBOLO",
+  "activitie": ["walk", "play", "run"]
+}
+
+try:
+ # myFirstCollection.insert_one(document)
+  result = myFirstCollection.find()
+  for document_extracted in result:
+    print(document_extracted)
+except Exception as error:
+  print(error)
+
+
+
+
